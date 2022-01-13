3f0686dcfca1afb609ad505a3d028cda9a410ce9
grig9 <70834200+grig9@users.noreply.github.com>
2022-01-13 11:26:59 +0300

    3

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php 
 
 function get_user_by_email($email) {
-include "connect_db.php";
+  include "connect_db.php";
 
-$sql = "SELECT * FROM users WHERE email = ?";
+  $sql = "SELECT * FROM users WHERE email = ?";
   $statement = $pdo->prepare($sql);
   $statement->execute([$email]);
   $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-return $user;
+  return $user;
 }
 
 function add_user($email, $password) {
