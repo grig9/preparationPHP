6a4d373d7da15e23a5825270e13e86aea69bd9b7
grig9 <70834200+grig9@users.noreply.github.com>
2022-01-13 09:15:01 +0300

    fgsdf

---
+++ connect_db.php
@@ -0,0 +1,12 @@
+<?php
+
+$driver = 'mysql'; // тип базы данных, с которой мы будем работать 
+$host = 'localhost';// альтернатива '127.0.0.1' - адрес хоста, в нашем случае локального
+$db_name = 'lesson_php'; // имя базы данных 
+$db_user = 'root'; // имя пользователя для базы данных 
+$db_password = ''; // пароль пользователя 
+$charset = 'utf8'; // кодировка по умолчанию 
+
+$dsn = "$driver:host=$host;dbname=$db_name;charset=$charset";
+
+$pdo = new PDO($dsn, $db_user, $db_password); 
--- functions.php
+++ functions.php
@@ -1,4 +1,20 @@
+<?php 
 
-function get_user_by_email() {
-    
+
+
+function get_user_by_email($email) {
+	include "connect_db.php";
+	
+	$sql = "SELECT * FROM users WHERE email=:email";
+
+  $statement = $pdo->prepare($sql);
+  $statement->execute($_POST);
+  $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+	return $user;
 }
+
+
+
+
+
